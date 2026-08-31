d6117785f30a73cd2c2fc10f9d0395338070cdaa
Jaz Villanueva <jazvvillanueva@gmail.com>
2026-09-01 02:32:46 +0800

    chore: fix .gitignore and remove tracked build artifacts (.next/, tsconfig.tsbuildinfo)

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/SpaceScene\":{\"id\":\"app\\\\page.tsx -> @/components/SpaceScene\",\"files\":[\"static/chunks/_app-pages-browser_components_SpaceScene_tsx.js\"]}}"
